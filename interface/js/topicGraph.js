72898584e954493dd1e10229ed02870d21bcbd3e
Samuel Rönnqvist <samuel.ronnqvist@abo.fi>
2015-07-17 01:20:47 +0300

    first release

--- interface/js/topicGraph.js
+++ interface/js/topicGraph.js
@@ -1,4 +1,4 @@
-/* topicGraph - (c) 2014 Samuel Ronnqvist - sronnqvi@abo.fi */
+/* topicGraph - (c) 2014-15 Samuel Ronnqvist - sronnqvi@abo.fi */
 
 
 
@@ -7,7 +7,7 @@ var width = window.innerWidth-4,
 
 function color(h){return Color.hsl(h,0.7,0.4)}
 
-function prob2opacity(p){return Math.min(p*1.5,1)}
+function prob2opacity(p){ return (p)*0.8+0.1; }
 
 function neighboring(a, b) {
     var n = neighbors[a];
@@ -17,39 +17,55 @@ function neighboring(a, b) {
 
 function node_focus(cnode){
     gnodes
-        .style("font-size",function(o){
+        .attr("fill",function(o){
             if(neighboring(cnode.index, o.index)){
                 if(o.group == 1 && o.index != cnode.index){
                     var size = node_weights[[o.name, cnode.name]]*16+7;
-                    return(Math.round(size,2)+"pt");
+                    return("#000");//#fff");
                 } //else return("9pt")
             } //else return "9pt";
         });
 
     // Change node opacities
-    gnodes.transition().delay(50).style("opacity", function(o) {
+    gnodes.transition().delay(50).attr("opacity", function(o) {
         if(neighboring(cnode.index, o.index)) return "0.9"
-        else if(o.group == 1){return "0.1"} else {return "0.1"};
+        else {return "0.2"};
     }).duration(200);
+    /*
+    gnodes.selectAll("text").transition().delay(50).attr("font-weight", function(o) {
+        if(neighboring(cnode.index, o.index)) return "bold";
+        else {return "normal"};
+    }).duration(200);*/
 
-    d3.select(this).style("stroke-opacity","1");
+    d3.select(this).attr("stroke-opacity","1");
 
     // Change link opacities
     var retval;
-    link.transition().delay(50).style('stroke-opacity', function(l) {
+    link.transition().delay(50).attr('opacity', function(l) {
         if(cnode == l.source){
-            retval = prob2opacity(l.value);
+            retval = 0.8;
         } else if(cnode == l.target){
-            retval = prob2opacity(l.value);
+            retval = 0.8;
         } else {
             retval = 0.2;
         }
         return retval;
-    }).duration(200);
+        })
+        /*.attr('stroke', function(l) {
+            if(cnode == l.source){
+                retval = "#acf";
+            } else if(cnode == l.target){
+                retval = "#acf";
+            } else {
+                retval = "#666";
+            }
+            return retval;
+        })*/
+        .duration(200);
 }
 
 function link_focus(clink){
-    link.style('stroke-opacity', function(l) {
+    link.attr('opacity', function(l) {
         if(clink.source.index != l.source.index && clink.target.index != l.target.index){
             return 0.15;
         } else return prob2opacity(l.value);
@@ -60,13 +76,15 @@ function unfocus(){
     if(mouseDown) return;
     gn = d3.selectAll(".gnode");
     gn.transition()
-        .style("opacity","1.0")
+        .attr("opacity","1.0")
         .duration(200);
+    /*gn.selectAll("text")
+        .attr("font-weight","normal");*/
 
     gn.style('font-size','9pt');
 
     link.transition()
-        .style('stroke-opacity', function(l) {
+        .attr('opacity', function(l) {
             return prob2opacity(l.value);
         })
         .duration(200);
@@ -87,13 +105,14 @@ function node_select(cnode){
         nd = d3.select("#node_"+n)
         if(n[0] == 'T'){
             // Topic nodes
-            nd.style("fill",function(d){ return Color.hsl(d.topic/ntopics, 0.7, 0.2) });
+            nd.attr("fill",function(d){ return Color.hsl(d.topic/ntopics, 0.7, 0.2) });
         } else {
             // Keyword nodes
             nd.select("text")
                 .style("text-decoration","underline")
                 .style("font-weight","bold")
-                .style("font-color","#eee");
+                //.style("font-color","#eee");
+                .style("font-color","#111");
         }
     }
 
@@ -106,7 +125,7 @@ function node_select(cnode){
         nd = d3.select("#node_"+n);
         if(n[0] == 'T'){
             // Topic nodes
-            nd.style("fill", "black");
+            nd.attr("fill", "black");
         } else {
             // Keyword nodes
             nd.select("text")
@@ -145,16 +164,23 @@ function link_select(link){
     // Not implemented / needed
 }
 
+function linkStrengthFunc(d){
+    return d.value*0.7+0.3;
+}
+
 var force = d3.layout.force()
-    .charge(-750)
-    .linkDistance(function(d,i) {
+    .charge(-200)
+    /*.linkDistance(function(d,i) {
         termDegree = _.filter(links, function(x){return(x.source.index == i || x.target.index == i)}).length;
         return ((180-d.value*100)*Math.pow(termDegree), 1.2);
-    })
+    })*/
+    .linkDistance(150)
     .size([width, height])
-    .gravity(0.01)
-    .friction(0.3)
-    .linkStrength(0.4);
+    .gravity(0.025)
+    .friction(0.9)
+    .linkStrength(function(d){
+        return Math.pow(d.value,2)*1.2;
+    });
 
 var mouseDown = 0;
 document.body.onmousedown = function() {
@@ -167,6 +193,8 @@ document.body.onmouseup = function() {
 
 var nodes, links, node, link, gnodes, neighbors = {}, node_weights = {}, selected = {}, ntopics, node_names;
 
+var zoomer = d3.behavior.zoom().scaleExtent([-8, 8]).on("zoom", zoom);
+
 var svg = d3.select("body")
     .append("svg")
     .attr("id", "svgRoot")
@@ -175,16 +203,16 @@ var svg = d3.select("body")
     .attr("xmlns", "http://www.w3.org/2000/svg")
     .attr("version", 1.1)
     .append("g")
-    .call(d3.behavior.zoom().scaleExtent([-8, 8]).on("zoom", zoom))
+    .call(zoomer)
     .append("g")
     .attr("id", "allContent");
 
 svg.append("rect")
     .attr("class", "overlay")
-    .attr("width", width*3)
-    .attr("height", height*3)
-    .attr("x",-1*width)
-    .attr("y",-1*height)
+    .attr("width", width*10)
+    .attr("height", height*10)
+    .attr("x",-5*width)
+    .attr("y",-5*height)
     .attr("stroke","#aaa")
     .attr("fill", "none");
 
@@ -192,10 +220,20 @@ function zoom() {
     svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 }
 
+zoomer.scale(0.2);
+zoomer.translate([width*0.4,height*0.4]);
+zoomer.event(svg);
+
+function zoomBy(factor){
+    zoomer.scale(zoomer.scale()*factor);
+    //zoomer.translate([width*0.45,height*0.45]);
+    zoomer.event(svg);
+}
+
 nodes = [];
 links = [];
 
-var graph = d3.json("data/graph"+window.location.search.substring(1)+".json", function(error, graph) {
+var graph = d3.json("data/graph.json", function(error, graph) {
     nodes = graph.nodes;
     links = graph.links;
 
@@ -204,19 +242,21 @@ var graph = d3.json("data/graph"+window.location.search.substring(1)+".json", fu
         .links(graph.links)
         .start();
 
-    ntopics = _.filter(nodes, function(x){return x.group == 0}).length
+    ntopics = _.filter(nodes, function(x){return x.group == 0}).length;
 
     link = svg.selectAll(".link")
         .data(graph.links)
         .enter().append("line")
         .attr("class", "link")
-        .style("stroke-width", function(d) { return 2.2; })
-        .style("stroke", function(d,i) {
-            if(nodes[d.source.index].group == 0) return color(nodes[d.source.index].topic/ntopics)
-            else if(nodes[d.target.index].group == 0) return color(nodes[d.target.index].topic/ntopics)
-            else return "black";
+        .attr("stroke-width", function(d) { return 2.2; })
+        .attr("stroke", function(d,i) {
+            return "#024";
+/*            //if(d.value > 0.7) return "#24e";
+            if(nodes[d.source.index].group == 0) return "#aaa";//"#666";//color(nodes[d.source.index].topic/ntopics)
+            else if(nodes[d.target.index].group == 0) return "#aaa";//"#666";//color(nodes[d.target.index].topic/ntopics)
+            else return "#000";//"#fff";*/
         })
-        .style("stroke-opacity", function(d) { return prob2opacity(d.value); });
+        .attr("opacity", function(d) { return prob2opacity(d.value) });
 
     var drag = force.drag()
         .on("dragstart", dragstarted)
@@ -253,31 +293,46 @@ var graph = d3.json("data/graph"+window.location.search.substring(1)+".json", fu
             if(d.group == 0) return Math.pow(d.prop,0.9)*200; //d.prop*200 //50*Math.sqrt(d.prop/3.1415926)
             else return 5;
         })
-        .style("opacity", function(d){
+        .attr("opacity", function(d){
             if(d.group == 0) return 0.9;
             else return 0.3;
         })
-        .style("stroke", function(d,i) {
+        .attr("stroke", function(d,i) {
             if(d.group == 0)
                 return color(d.topic/ntopics);
             else return "white";
         })
-        .style("stroke-width", "2px");
+        .attr("stroke-width", "2px");
 
     gnodes.append("text")
         .attr("text-anchor", "middle")
         .attr("dy",".35em")
         .text(function(d) { return d.name; })
-        .style("fill", function(d){
+        .attr("fill", function(d){
             if(d.group==0)
-                return(color(d.topic/ntopics))
+                return("#000");//"#e0e0e0");//return(color(d.topic/ntopics));
             else
-                return("#eee");
+                return("#888");
         })
         .style("font-weight", function(d){
-            if(d.group==0)
+            if(false && d.group==0)
                 return("bold");
             else return("normal");
+        })
+        .style("font-size", function(d){
+            if(d.group==0){
+                console.log(d.prop);
+                //return((8+Math.log(1+d.prop,2)*6000)+"pt");
+                return((Math.log(d.prop)+10)*10+"pt");
+            } else {
+                return("10pt");
+            }
+        })
+        .on("mouseenter", function(){
+            d3.select(this).attr("fill", "#003")//#fff")
+        })
+        .on("mouseleave", function(){
+            d3.select(this).attr("fill", "#000")//#e0e0e0")
         });
 
     force.on("tick", function() {
@@ -296,10 +351,10 @@ var graph = d3.json("data/graph"+window.location.search.substring(1)+".json", fu
 
     links.forEach(function(l){
         if(neighbors[l.source.index] == undefined)
-            neighbors[l.source.index] = [l.source.index, l.target.index]
+            neighbors[l.source.index] = [l.source.index, l.target.index];
         else neighbors[l.source.index].push(l.target.index);
         if(neighbors[l.target.index] == undefined)
-            neighbors[l.target.index] = [l.target.index, l.source.index]
+            neighbors[l.target.index] = [l.target.index, l.source.index];
         else neighbors[l.target.index].push(l.source.index);
 
         if(l.source.group == 0){
@@ -316,18 +371,17 @@ var graph = d3.json("data/graph"+window.location.search.substring(1)+".json", fu
 
 });
 
-/*
-TODO: Implement information retrieval functionality by linking topics/keywords to documents
-var topic2docs, word2docs, titles;
-var docs = d3.json("docs.json", function(error, docdata) {
-    topic2docs = docdata.topic2docs;
-    word2docs = docdata.word2docs;
-    titles = docdata.titles;
-});
-*/
 
 setTimeout(function(){
     var header = d3.selectAll(".header");
     header.transition().duration(2000).style("opacity", 0);
     header.transition().style("display", "none").delay(2000);
 }, 6000);
+
+setTimeout(function(){
+    force.charge(-800).start();
+}, 200);
+setTimeout(function(){
+    force.charge(-1200).linkStrength(linkStrengthFunc).start();
+}, 600);
+
--- modeling/build_graph.py
+++ modeling/build_graph.py
@@ -6,6 +6,18 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import pylab
 import json
+import numpy
+import logging
+
+# Network construction parameters
+#   number of most frequent terms, N
+topn = 500
+#   threshold percentile, P
+th = 98.5
+#   max links per node, L
+node_lim = 12
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
 if len(sys.argv) < 2:
     print "Usage: %s <corpus name>" % sys.argv[0]
@@ -15,151 +27,83 @@ if len(sys.argv) < 2:
 dictionary = corpora.Dictionary.load(sys.argv[1]+".dict")
 corpus = corpora.MmCorpus(sys.argv[1]+".mm")
 
-# Define number of topics
-ntopics = 10
+stopwords = set(map(str.strip, open("stopwords.list").read().split('\n')))
 
-# Train topic model
-model = models.LdaModel(corpus, id2word=dictionary, num_topics=ntopics, alpha=1./ntopics, eta=1./ntopics)
+print "Loading semantic model..."
+sem_model = models.Word2Vec.load("%s.w2v" % sys.argv[1])
 
-### Prob. rescaling, P(t|w)
 termcntr = collections.defaultdict(lambda:0)
-topiccntr = collections.defaultdict(lambda:0)
 
 for doc in corpus:
     for term, freq in doc:
         termcntr[term] += freq
-    for topic, p in model[doc]:
-        topiccntr[topic] += p
 
 termtot = float(sum(termcntr.values()))
-topictot = len(corpus)
 
-topics = model.show_topics(num_topics=-1, num_words=50, formatted=False)
+revdict = dict([(b, a) for a, b in dictionary.items()])
 
-#P(t|w)
-ptw = collections.defaultdict(lambda: {})
+top_terms = [(dictionary[x[0]], x[1]/termtot) for x in sorted(termcntr.items(), key=lambda x: x[1])[-1*topn:]
+             if dictionary[x[0]] not in stopwords and len(dictionary[x[0]]) > 1 and
+                not dictionary[x[0]].isdigit() and x[1] > 2]
 
-for topic, i in zip(topics, range(ntopics)):
-    neworder = []
-    for pwt, term in topic:
-        term_id = dictionary.token2id[term]
-        if termcntr[term_id] < 30:
-            continue
-        #pwt: p(w|t)
-        pw = termcntr[dictionary.token2id[term]]/termtot #p(w)
-        pt = topiccntr[i]/topictot #p(t)
-        ptw[i][term_id] = pwt*pt/pw #p(t|w)
-        neworder.append((ptw[i][term_id], term))
-    neworder.sort(reverse=True)
-    for p, term in neworder[:15]:
-        print term, "\t", p
-    print
-
-
-### Probabilistic document-representative word scoring
-
-# P(d)
-pd = 1./len(corpus)
-pw = collections.defaultdict(lambda: 0)
-pwd = collections.defaultdict(lambda: {})
-for doc, doc_id in zip(corpus,range(len(corpus))):
-    doc_len = sum([x[1] for x in doc])
-    for term_id, cnt in doc:
-        # Set P(w|d) for current w
-        pwd[doc_id][term_id] = cnt/doc_len
-        # P(w) update
-        pw[term_id] += cnt
-
-
-pwtot = sum(pw.values())
-for w in pw:
-    pw[w] = pw[w]/pwtot
-
-# P(d|w)
-pdw = collections.defaultdict(lambda: {})
-for doc_id in range(len(corpus)):
-    for w in pwd[doc_id]:
-        pdw[doc_id][w] = pwd[doc_id][w] * pd / pw[w]        
-
-# Distinctiveness
-# sum_T(P(T|w)log(P(T|w)/P(T))
 
 visdict = set()
-
 g = nx.Graph()
-for topic, topicnr in zip(topics, range(len(topics))):
-    target = 'T%d' % topicnr
-    g.add_node(target, {'type': 'topic'})
-    for w in ptw[topicnr]:
-        prob = ptw[topicnr][w]
-        if prob < 0.13:
-            continue
-        source = dictionary[w]
-        visdict.add(source)
-        g.add_edge(source, target, {'prob': prob})
-        
 
-topicsize = collections.defaultdict(lambda: 0)
-for doc in corpus:
-    for topic, prob in model[doc]:
-        topicsize[topic] += prob
+bucket = []
+terms = [t for t, w in top_terms]
+for t_i, t1 in enumerate(terms):
+    g.add_node(t1)
+    queue = []
+    for t2 in terms[(t_i+1):]:
+        try:
+            sim = sem_model.similarity(t1, t2)
+        except KeyError:
+            sim = 0
+        if sim > 0.0:
+            #print t1, t2, sim
+            queue.append((sim, t2))
+            #for tx, w in [x for x in sem_model.most_similar([t1,t2], topn=20) if termcntr[revdict[x[0]]] > termcntr[revdict[top_terms[0][0]]] and x[1] > 0.6]:
+            #    g.add_edge(t1 +'+'+t2, tx, {'w': w})
+        bucket.append(sim)
+    print t1
+    for sim, t2 in sorted(queue)[-50:]:
+        #print t2,
+        g.add_edge(t1, t2, {'w': sim})
+    #print
 
-totsize = sum(topicsize.values())
-for topic in topicsize:
-    topicsize[topic] /= totsize
+
+limit = numpy.percentile(bucket, th)
+
+weights = [x for x in bucket if x > limit]
+w_max = max(weights)
+w_min = min(weights)
+normalize = lambda w: (w-w_min)/(w_max-w_min)
+
+term_weights = collections.defaultdict(lambda: 0)
+term_weights.update(dict(top_terms))
+
+for node, edges in g.edge.items():
+    if len(edges) > node_lim:
+        for node2, data in sorted(g.edge[node].items(),key=lambda x: x[1]['w'])[:(len(edges)-node_lim)]:
+            g.remove_edge(node, node2)
+
+connected_nodes = set(reduce(lambda a,b: a+b, [[x[0], x[1]] for x in g.edges(data=True) if x[2]['w'] > limit]))
 
 jsondata = {}
-jsondata['nodes'] = [{'name': x[0], 
-                      'group': 0 if 'type' in x[1] else 1, 
-                      'prop': topicsize[int(x[0][1:])] if 'type' in x[1] else 0,
-                      'topic': int(x[0][1:]) if 'type' in x[1] else 'NA'} for x in g.nodes(data=True)]
+jsondata['nodes'] = [{'name': x[0],
+                      'group': x[1]['group'] if 'group' in x[1] else 0,
+                      'prop': term_weights[x[0]]
+                     } for x in g.nodes(data=True) if x[0] in connected_nodes]
 node_id_lookup = dict(zip([x['name'] for x in jsondata['nodes']], range(len(jsondata['nodes']))))
-jsondata['links'] = [{'source': node_id_lookup[x[0]], 'target': node_id_lookup[x[1]], 'value': x[2]['prob']} for x in g.edges(data=True)]
+jsondata['links'] = [{'source': node_id_lookup[x[0]], 'target': node_id_lookup[x[1]], 'value': normalize(x[2]['w'])} for x in g.edges(data=True) if x[2]['w'] > limit]
 
-
-open("graph.json","w").write(repr(jsondata)\
-    .replace("'",'"')\
-    .replace("},","},\n\t")\
-    .replace("],","],\n"))
-
-
-### Document look-up
-
-"""
-#Not implemented in front end
-
-topic2docs = collections.defaultdict(lambda: [])
-word2docs = collections.defaultdict(lambda: [])
-for i in range(len(corpus)):
-    for assign, prob in model[corpus[i]]:
-        if prob > 0.1:
-            topic2docs[assign].append((i,round(prob,5)))
-    wordcnt = sum([x[1] for x in corpus[i]])
-    for term_id, count in corpus[i]:
-        term = dictionary[term_id]
-        if term in visdict:
-            prob = count/wordcnt
-            if prob > 0.01:
-                word2docs[term].append((i, round(prob, 5)))
-
-
-txtdata = open(sys.argv[1] + '.txt')
-titles = []
-while True:
-    line = txtdata.readline()
-    if line == "":
-        break
+for i, _ in enumerate(jsondata['nodes']):
     try:
-        line = line.split(' . ')[0]
+        jsondata['nodes'][i]['name'] = jsondata['nodes'][i]['name'].encode('utf-8')
     except:
-        pass
-    line = line[0].upper() + line[1:].lower()
-    titles.append(line)
+        jsondata['nodes'][i]['name'] = "///"
 
+open("graph.json","w").write(json.dumps(jsondata))
 
-titles = [title.replace('"', "").replace("'",'') for title in titles]
-
-open("docs.json","w").write(json.dumps({'topic2docs': dict(topic2docs), 'word2docs': dict(word2docs), 'titles':titles}))
-
-"""
-
+### TODO: Sem model on corpus to reflext local contexts, focus by most_similar
