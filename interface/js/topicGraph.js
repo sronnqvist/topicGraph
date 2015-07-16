47ac85e4a54b5775d7aa349fdd2c33b3b0ddc524
Samuel Rönnqvist <samuel.ronnqvist@abo.fi>
2015-07-17 01:28:15 +0300

    Revert "first release"
    This reverts commit af7cd8e3e97eeaef003711033214eabd4cd038ab.

--- interface/js/topicGraph.js
+++ interface/js/topicGraph.js
@@ -1,4 +1,4 @@
-/* topicGraph - (c) 2014-15 Samuel Ronnqvist - sronnqvi@abo.fi */
+/* topicGraph - (c) 2014 Samuel Ronnqvist - sronnqvi@abo.fi */
 
 
 
@@ -7,7 +7,7 @@ var width = window.innerWidth-4,
 
 function color(h){return Color.hsl(h,0.7,0.4)}
 
-function prob2opacity(p){ return (p)*0.8+0.1; }
+function prob2opacity(p){return Math.min(p*1.5,1)}
 
 function neighboring(a, b) {
     var n = neighbors[a];
@@ -17,55 +17,39 @@ function neighboring(a, b) {
 
 function node_focus(cnode){
     gnodes
-        .attr("fill",function(o){
+        .style("font-size",function(o){
             if(neighboring(cnode.index, o.index)){
                 if(o.group == 1 && o.index != cnode.index){
                     var size = node_weights[[o.name, cnode.name]]*16+7;
-                    return("#000");//#fff");
+                    return(Math.round(size,2)+"pt");
                 } //else return("9pt")
             } //else return "9pt";
         });
 
     // Change node opacities
-    gnodes.transition().delay(50).attr("opacity", function(o) {
+    gnodes.transition().delay(50).style("opacity", function(o) {
         if(neighboring(cnode.index, o.index)) return "0.9"
-        else {return "0.2"};
+        else if(o.group == 1){return "0.1"} else {return "0.1"};
     }).duration(200);
-    /*
-    gnodes.selectAll("text").transition().delay(50).attr("font-weight", function(o) {
-        if(neighboring(cnode.index, o.index)) return "bold";
-        else {return "normal"};
-    }).duration(200);*/
 
-    d3.select(this).attr("stroke-opacity","1");
+    d3.select(this).style("stroke-opacity","1");
 
     // Change link opacities
     var retval;
-    link.transition().delay(50).attr('opacity', function(l) {
+    link.transition().delay(50).style('stroke-opacity', function(l) {
         if(cnode == l.source){
-            retval = 0.8;
+            retval = prob2opacity(l.value);
         } else if(cnode == l.target){
-            retval = 0.8;
+            retval = prob2opacity(l.value);
         } else {
             retval = 0.2;
         }
         return retval;
-        })
-        /*.attr('stroke', function(l) {
-            if(cnode == l.source){
-                retval = "#acf";
-            } else if(cnode == l.target){
-                retval = "#acf";
-            } else {
-                retval = "#666";
-            }
-            return retval;
-        })*/
-        .duration(200);
+    }).duration(200);
 }
 
 function link_focus(clink){
-    link.attr('opacity', function(l) {
+    link.style('stroke-opacity', function(l) {
         if(clink.source.index != l.source.index && clink.target.index != l.target.index){
             return 0.15;
         } else return prob2opacity(l.value);
@@ -76,15 +60,13 @@ function unfocus(){
     if(mouseDown) return;
     gn = d3.selectAll(".gnode");
     gn.transition()
-        .attr("opacity","1.0")
+        .style("opacity","1.0")
         .duration(200);
-    /*gn.selectAll("text")
-        .attr("font-weight","normal");*/
 
     gn.style('font-size','9pt');
 
     link.transition()
-        .attr('opacity', function(l) {
+        .style('stroke-opacity', function(l) {
             return prob2opacity(l.value);
         })
         .duration(200);
@@ -105,14 +87,13 @@ function node_select(cnode){
         nd = d3.select("#node_"+n)
         if(n[0] == 'T'){
             // Topic nodes
-            nd.attr("fill",function(d){ return Color.hsl(d.topic/ntopics, 0.7, 0.2) });
+            nd.style("fill",function(d){ return Color.hsl(d.topic/ntopics, 0.7, 0.2) });
         } else {
             // Keyword nodes
             nd.select("text")
                 .style("text-decoration","underline")
                 .style("font-weight","bold")
-                //.style("font-color","#eee");
-                .style("font-color","#111");
+                .style("font-color","#eee");
         }
     }
 
@@ -125,7 +106,7 @@ function node_select(cnode){
         nd = d3.select("#node_"+n);
         if(n[0] == 'T'){
             // Topic nodes
-            nd.attr("fill", "black");
+            nd.style("fill", "black");
         } else {
             // Keyword nodes
             nd.select("text")
@@ -164,23 +145,16 @@ function link_select(link){
     // Not implemented / needed
 }
 
-function linkStrengthFunc(d){
-    return d.value*0.7+0.3;
-}
-
 var force = d3.layout.force()
-    .charge(-200)
-    /*.linkDistance(function(d,i) {
+    .charge(-750)
+    .linkDistance(function(d,i) {
         termDegree = _.filter(links, function(x){return(x.source.index == i || x.target.index == i)}).length;
         return ((180-d.value*100)*Math.pow(termDegree), 1.2);
-    })*/
-    .linkDistance(150)
+    })
     .size([width, height])
-    .gravity(0.025)
-    .friction(0.9)
-    .linkStrength(function(d){
-        return Math.pow(d.value,2)*1.2;
-    });
+    .gravity(0.01)
+    .friction(0.3)
+    .linkStrength(0.4);
 
 var mouseDown = 0;
 document.body.onmousedown = function() {
@@ -193,8 +167,6 @@ document.body.onmouseup = function() {
 
 var nodes, links, node, link, gnodes, neighbors = {}, node_weights = {}, selected = {}, ntopics, node_names;
 
-var zoomer = d3.behavior.zoom().scaleExtent([-8, 8]).on("zoom", zoom);
-
 var svg = d3.select("body")
     .append("svg")
     .attr("id", "svgRoot")
@@ -203,16 +175,16 @@ var svg = d3.select("body")
     .attr("xmlns", "http://www.w3.org/2000/svg")
     .attr("version", 1.1)
     .append("g")
-    .call(zoomer)
+    .call(d3.behavior.zoom().scaleExtent([-8, 8]).on("zoom", zoom))
     .append("g")
     .attr("id", "allContent");
 
 svg.append("rect")
     .attr("class", "overlay")
-    .attr("width", width*10)
-    .attr("height", height*10)
-    .attr("x",-5*width)
-    .attr("y",-5*height)
+    .attr("width", width*3)
+    .attr("height", height*3)
+    .attr("x",-1*width)
+    .attr("y",-1*height)
     .attr("stroke","#aaa")
     .attr("fill", "none");
 
@@ -220,20 +192,10 @@ function zoom() {
     svg.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
 }
 
-zoomer.scale(0.2);
-zoomer.translate([width*0.4,height*0.4]);
-zoomer.event(svg);
-
-function zoomBy(factor){
-    zoomer.scale(zoomer.scale()*factor);
-    //zoomer.translate([width*0.45,height*0.45]);
-    zoomer.event(svg);
-}
-
 nodes = [];
 links = [];
 
-var graph = d3.json("data/graph.json", function(error, graph) {
+var graph = d3.json("data/graph"+window.location.search.substring(1)+".json", function(error, graph) {
     nodes = graph.nodes;
     links = graph.links;
 
@@ -242,21 +204,19 @@ var graph = d3.json("data/graph.json", function(error, graph) {
         .links(graph.links)
         .start();
 
-    ntopics = _.filter(nodes, function(x){return x.group == 0}).length;
+    ntopics = _.filter(nodes, function(x){return x.group == 0}).length
 
     link = svg.selectAll(".link")
         .data(graph.links)
         .enter().append("line")
         .attr("class", "link")
-        .attr("stroke-width", function(d) { return 2.2; })
-        .attr("stroke", function(d,i) {
-            return "#024";
-/*            //if(d.value > 0.7) return "#24e";
-            if(nodes[d.source.index].group == 0) return "#aaa";//"#666";//color(nodes[d.source.index].topic/ntopics)
-            else if(nodes[d.target.index].group == 0) return "#aaa";//"#666";//color(nodes[d.target.index].topic/ntopics)
-            else return "#000";//"#fff";*/
+        .style("stroke-width", function(d) { return 2.2; })
+        .style("stroke", function(d,i) {
+            if(nodes[d.source.index].group == 0) return color(nodes[d.source.index].topic/ntopics)
+            else if(nodes[d.target.index].group == 0) return color(nodes[d.target.index].topic/ntopics)
+            else return "black";
         })
-        .attr("opacity", function(d) { return prob2opacity(d.value) });
+        .style("stroke-opacity", function(d) { return prob2opacity(d.value); });
 
     var drag = force.drag()
         .on("dragstart", dragstarted)
@@ -293,46 +253,31 @@ var graph = d3.json("data/graph.json", function(error, graph) {
             if(d.group == 0) return Math.pow(d.prop,0.9)*200; //d.prop*200 //50*Math.sqrt(d.prop/3.1415926)
             else return 5;
         })
-        .attr("opacity", function(d){
+        .style("opacity", function(d){
             if(d.group == 0) return 0.9;
             else return 0.3;
         })
-        .attr("stroke", function(d,i) {
+        .style("stroke", function(d,i) {
             if(d.group == 0)
                 return color(d.topic/ntopics);
             else return "white";
         })
-        .attr("stroke-width", "2px");
+        .style("stroke-width", "2px");
 
     gnodes.append("text")
         .attr("text-anchor", "middle")
         .attr("dy",".35em")
         .text(function(d) { return d.name; })
-        .attr("fill", function(d){
+        .style("fill", function(d){
             if(d.group==0)
-                return("#000");//"#e0e0e0");//return(color(d.topic/ntopics));
+                return(color(d.topic/ntopics))
             else
-                return("#888");
+                return("#eee");
         })
         .style("font-weight", function(d){
-            if(false && d.group==0)
+            if(d.group==0)
                 return("bold");
             else return("normal");
-        })
-        .style("font-size", function(d){
-            if(d.group==0){
-                console.log(d.prop);
-                //return((8+Math.log(1+d.prop,2)*6000)+"pt");
-                return((Math.log(d.prop)+10)*10+"pt");
-            } else {
-                return("10pt");
-            }
-        })
-        .on("mouseenter", function(){
-            d3.select(this).attr("fill", "#003")//#fff")
-        })
-        .on("mouseleave", function(){
-            d3.select(this).attr("fill", "#000")//#e0e0e0")
         });
 
     force.on("tick", function() {
@@ -351,10 +296,10 @@ var graph = d3.json("data/graph.json", function(error, graph) {
 
     links.forEach(function(l){
         if(neighbors[l.source.index] == undefined)
-            neighbors[l.source.index] = [l.source.index, l.target.index];
+            neighbors[l.source.index] = [l.source.index, l.target.index]
         else neighbors[l.source.index].push(l.target.index);
         if(neighbors[l.target.index] == undefined)
-            neighbors[l.target.index] = [l.target.index, l.source.index];
+            neighbors[l.target.index] = [l.target.index, l.source.index]
         else neighbors[l.target.index].push(l.source.index);
 
         if(l.source.group == 0){
@@ -371,17 +316,18 @@ var graph = d3.json("data/graph.json", function(error, graph) {
 
 });
 
+/*
+TODO: Implement information retrieval functionality by linking topics/keywords to documents
+var topic2docs, word2docs, titles;
+var docs = d3.json("docs.json", function(error, docdata) {
+    topic2docs = docdata.topic2docs;
+    word2docs = docdata.word2docs;
+    titles = docdata.titles;
+});
+*/
 
 setTimeout(function(){
     var header = d3.selectAll(".header");
     header.transition().duration(2000).style("opacity", 0);
     header.transition().style("display", "none").delay(2000);
 }, 6000);
-
-setTimeout(function(){
-    force.charge(-800).start();
-}, 200);
-setTimeout(function(){
-    force.charge(-1200).linkStrength(linkStrengthFunc).start();
-}, 600);
-
--- modeling/convert_corpus.py
+++ modeling/convert_corpus.py
@@ -1,48 +1,19 @@
 #!/usr/bin/python
 
 import sys, re
-from gensim import corpora, models
-import unidecode
-import logging
-import numpy
-import collections
-import time
+from gensim import corpora
+import nltk.tokenize.punkt as punkt
+# Alt: use TextBlob for tokenization
 
+tokenizer = punkt.PunktWordTokenizer()
 
-class MyDocs(object):
-    def __init__(self, filename):
-        self.filename = filename
-    def __iter__(self):
-        for line in open(self.filename):
-            line = unidecode.unidecode(line.decode('utf-8'))
-            line = line.replace('`','\'').replace('\'', ' \' ')
-            line = line.replace('/',' / ')
-            line = re.sub("\d+([\-,\.]\d+)+", "000", line)
-            line = re.sub("\-{2,}", "", line)
-            line = re.sub("(^\-|\-$)", "", line)
-            line = re.sub("(\s\-|\-\s)", " ", line)
-            line = line.replace(":", " : ")
-            line = line.replace(",", " , ")
-            line = filter(lambda x: x.isalpha() or x.isdigit() or x in ".- ", line)
-            line = re.sub("\.\s", " ", line)
-            line = line.split()
-            line = map(lambda x: x.lower(), line)
-            yield line
-
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-print "Reading corpus..."
-texts = MyDocs(sys.argv[1])
-texts = [text for text in texts]
-
-print "Detecting phrases..."
-bigram = models.Phrases(texts)
-models.phrases.prune_vocab(bigram.vocab, numpy.percentile(bigram.vocab.values(), 90))
-
-texts = [[token.replace('_', ' ') for token in bigram[text]] for text in texts]
+# Read corpus file, tokenize, normalize
+texts = [[term.lower() for term in [re.sub("\.$", "", token) for token in tokenizer.tokenize(doc)]] 
+        for doc in open(sys.argv[1]).read().decode('utf-8').split('\n')]
+# Filter non-word tokens
+texts = [filter(lambda x: (len(x) > 1 or x.isalpha()) and not x.isdigit(), text) for text in texts]
 
 # Generate gensim dictionary
-print "Building dictionary..."
 dictionary = corpora.Dictionary(texts)
 
 # Save dictionary
@@ -53,33 +24,6 @@ else:
     exit()
 
 # Save converted corpus
-print "Building BOWs..."
 corpus = [dictionary.doc2bow(text) for text in texts]
 corpora.MmCorpus.serialize(sys.argv[2]+".mm", corpus)
-
-print "Building word vectors..."
-
-evaluate = False
-
-# Modeling parameters:
-#   size = vector size, V
-#   window = context size, C
-#   iter = epochs, E
-
-for size in [85]:
-    for window in [15]:
-        start = time.time()
-        print "size:",size, ", win:",window
-        iter = 10
-        logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-        model = models.Word2Vec(texts, min_count=1, workers=4, size=size, iter=iter, window=window)
-        for handler in logging.root.handlers[:]:
-            logging.root.removeHandler(handler)
-        print "size:",size, ", win:",window, ", time:",time.time()-start
-        if evaluate:
-            logging.basicConfig(format='size:%d,win:%d,iter:%d' % (size, window, iter) + ' %(message)s', level=logging.INFO, filename="accuracy.log", filemode='a')
-            ac = model.accuracy("questions.txt")
-        for handler in logging.root.handlers[:]:
-            logging.root.removeHandler(handler)
-        model.save(sys.argv[2]+".w2v")
 
